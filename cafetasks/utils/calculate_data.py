7b573e581ff41867857d72562aed3f784d33b97c
kyoredes <kyoredes@gmail.com>
2025-02-02 20:20:03 +1000

    utils fix: status object model change

--- cafetasks/utils/calculate_data.py
+++ cafetasks/utils/calculate_data.py
@@ -15,13 +15,17 @@ end_of_day = timezone.make_aware(
 
 def get_orders_count():
     return Order.objects.filter(
-        status=Status.PAID, created_at__gte=start_of_day, created_at__lt=end_of_day
+        status=Status.objects.filter(name="Оплачено").first(),
+        created_at__gte=start_of_day,
+        created_at__lt=end_of_day,
     ).count()
 
 
 def get_daily_revenue():
     orders = Order.objects.filter(
-        status=Status.PAID, created_at__gte=start_of_day, created_at__lt=end_of_day
+        status=Status.objects.filter(name="Оплачено").first(),
+        created_at__gte=start_of_day,
+        created_at__lt=end_of_day,
     ).aggregate(total_cost_sum=Sum("items__cost"))
 
     total_sum = orders["total_cost_sum"] or 0
